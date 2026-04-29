584febc7bf9e680e5e94a4217f74c7e012cbd34f
SIS <sergey.sergeev1000@gmail.com>
2026-04-29 17:16:26 +0300

    Add dirty hello appsec world py script

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S python3 -u
+# dirty demo code
+
+import sys,os
+
+def a( ):
+ print("Hello appsec world")
+ print("python:",sys.executable)
+ print("version:",sys.version.split()[0])
+
+if __name__=="__main__":
+ a()
